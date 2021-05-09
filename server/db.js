557685ad9d3bbdf98d798cad2c0a3641da9b2289
rywake <60336812+rywake@users.noreply.github.com>
2021-05-09 00:42:55 -0700

    Added SSL/TLS connection requirement to the DB.

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const fs = require('fs');
 require("dotenv").config({ path: __dirname + "/.env" });
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
@@ -6,6 +7,9 @@ const pool = mysql.createPool({
 	password: process.env.DB_PASS,
 	database: process.env.DB_DATABASE,
 	port: process.env.DB_PORT,
+	ssl  : {
+		ca : fs.readFileSync(__dirname + '/ssl_certificate/rds-ca-2019-root.pem')
+	  }
 });
 
 exports.getConnection = function (callback) {
