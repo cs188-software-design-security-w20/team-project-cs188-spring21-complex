43b8046e12830bbfdbf2d80c75697801fb1bef96
rywake <60336812+rywake@users.noreply.github.com>
2021-05-13 22:09:36 -0700

    Added photots for the apartments in westwood.

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -57,6 +57,56 @@ router.post("/user/:id", checkAuthentication, async function (req, res, next) {
 	});
 }});
 
+router.post("/apt/:id", checkAuthentication, async function (req, res, next) {
+	if (req.body.csrfToken !== getCsrfToken(req)) {
+		return res.json({ success: false, message: "Invalid CSRF Token" });
+	}
+	// TODO: Check session, ensure user is logged in
+	let file = req.files.image;
+	if (file !== undefined && (file.mimetype == "image/png" || file.mimetype == "image/jpeg")) {
+		// extension are stripped, mimetype checking should be sufficient
+		// generate a random UUID filename and move it to the destination
+		let uuid = uuidv4();
+		let new_path = path.join(process.env.UPLOAD_DIR, uuid);
+
+		let err = await file.mv(new_path);
+
+		if (err) {
+			console.log("ERROR: " + err);
+		}
+		console.log(`Saved ${file.name} to ${new_path}`);
+
+		dbConn.getConnection(async (err, db) => {
+			if (err) {
+				console.log("connection failed", err);
+				res.json({
+					success: false,
+					uuid: uuid,
+				});
+				return;
+			}
+			db.query(
+				`INSERT INTO apartment_image (apt_id, image_uuid) VALUES (${req.params['id']},'${uuid}')`,
+				(err, user) => {
+					db.release();
+					if (err) {
+						console.log(err);
+						res.json({
+							success: false,
+							message: err,
+						}); 
+					}
+					else
+					{
+						res.json({
+							success: true,
+							message: "success",
+						});
+					}
+		});
+	});
+}});
+
 // check that req.user is valid before user accesses some URL
 function checkAuthentication(req, res, next) {
 	// console.log("Checking if user is authenticated", req.sessionID, req.user);
